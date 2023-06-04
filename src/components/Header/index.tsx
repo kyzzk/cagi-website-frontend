5b4cbe0b95b728e95f24a39418a759548b5bf903
gabriel-andreoli <gabriandreoli@outlook.com>
2023-06-04 18:35:33 -0300

    nav and details

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import { FiMenu } from 'react-icons/fi';
 import { ShoppingCart, LogIn } from 'react-feather';
 import './Header.css';
+import logoCagi from '../../img/LogoCagiPreta.png';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Header: React.FC = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -11,18 +13,29 @@ const Header: React.FC = () => {
   };
 
   return (
-    <header className="header">
+    <header className="header py-4 shadow">
       <FiMenu className="hamburger" onClick={toggleMenu} />
       <nav className={`menu ${isOpen ? 'open' : ''}`}>
         <div className="left-side">
-          <a href='/'>CAGI</a>
-          <p>HOME</p>
-          <p>PRODUTOS</p>
-          <p>SOBRE O CURSO</p>
+          <a target="blank" href="https://www.instagram.com/cagiufpr/">
+            <img className="img-fluid logo-cagi" src={logoCagi} alt="Logo do CAGI" />
+          </a>
+          <a href='/' className='mx-2'>
+            <p>CAGI</p>
+          </a>
+          <a href="#" className='mx-2'>
+            <p>Home</p>
+          </a>
+          <a href="#" className='mx-2'>
+            <p>Produtos</p>
+          </a>
+          <a href="#" className='mx-2'>
+            <p>Sobre o curso</p>
+          </a>
         </div>
       </nav>
       <div className="right-side">
-          <LogIn />
+          <a href='Login'><LogIn /></a>
           <ShoppingCart />
         </div>
     </header>
