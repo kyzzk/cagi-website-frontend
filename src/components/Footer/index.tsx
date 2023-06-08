5b7ea8afb42081e868f499b5e833af89020bed4c
kaze <guillherme-morais@live.com>
2023-06-08 00:14:18 -0300

    Atualizações no footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,103 +1,56 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './Footer.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { Link } from 'react-router-dom'; 
+import { Facebook, Instagram, Twitter } from '@mui/icons-material';
 
-const Footer: React.FC = () => {
-  return (
-    <footer id="Footer" className="page-footer font-small stylish-color-dark pt-4">
-      <div className="container text-center text-md-left">
-        <div className="row">
-          <div className="col-md-4 mx-auto">
-            <h5 className="text-uppercase font-weight-bold mt-3 mb-4">Centro Acadêmico de Gestão da Informação</h5>
-            <hr className="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" style={{ width: '70px' }} />
-            <p>
-              Estamos sempre em busca para trazer a melhor experiência acadêmica para os nossos membros do curso de Gestão da Informação da UFPR!
-            </p>
-          </div>
-          <hr className="clearfix w-100 d-md-none" />
-          <div id="link10" className="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
-            <h6 className="text-uppercase font-weight-bold">Nossos Produtos</h6>
-            <hr className="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" style={{ width: '70px' }} />
-            <p>
-              <a href="#!">Camiseta</a>
-            </p>
-            <p>
-              <a href="#!">Moletom</a>
-            </p>
-          </div>
-          <hr className="clearfix w-100 d-md-none" />
-          <div id="link10" className="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
-            <h6 className="text-uppercase font-weight-bold">Links úteis</h6>
-            <hr className="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" style={{ width: '70px' }} />
-            <p>
-              <a href="http://www.sociaisaplicadas.ufpr.br/portal/decigi/">Decigi</a>
-            </p>
-            <p>
-              <a href="http://www.sociaisaplicadas.ufpr.br/portal/decigi/graduacao/matriz-e-ementas/">Grade curricular</a>
-            </p>
-          </div>
-          <hr className="clearfix w-100 d-md-none" />
-          <div className="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
-            <h6 className="text-uppercase font-weight-bold">Contatos</h6>
-            <hr className="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" style={{ width: '60px' }} />
-            <p>
-              <i className="fas fa-home mr-3"></i> cagiufpr@gmail.com
-            </p>
-            <p>
-              <i className="fas fa-envelope mr-3"></i> Av. Prefeito Lothário Meissner, 632 - Jardim Botânico
-            </p>
-            <p>
-              <i className="fas fa-phone mr-3"></i> 41 99999-9999
-            </p>
-          </div>
+
+const Footer = () => {
+
+    return (
+        <div className="footer">
+            <div className="left">
+                <h1 className="title-footer">CAGI</h1>
+                <p className="text" style={{color: '#A2A2A2'}} >Adquira produtos exclusivos de forma rápida!</p>
+                <div className="icons">
+                    <Twitter className="icon" />
+                    <Instagram className="icon" />
+                    <Facebook className="icon" />
+                </div>
+            </div>
+
+            <div className="center">
+                <p className="terms">Termos e Condições</p>
+                <p className="disclaimer">
+                Ao utilizar o site de vendas do CAGI, você concorda em cumprir as leis aplicáveis, fornecer informações precisas, e reconhece que o CAGI não se responsabiliza por danos decorrentes do uso do site. O CAGI reserva-se o direito de alterar os termos sem aviso prévio.
+                </p>
+                <p className="madeBy">
+                    <a style={{color: 'white'}} href="https://twitter.com/kazedgaf" target="_blank" rel="noopener noreferrer">
+                        Made with ❤️ by Guilherme and Gabriel
+                    </a>
+                </p>
+            </div>
+
+
+            <div className="right">
+                <div className="options">
+                    <h2 className='h2Styled'>Opções</h2>
+                    <ul>
+                        <li><Link style={{color: '#A2A2A2'}} to="/login">Logar</Link></li>
+                        <li><Link style={{color: '#A2A2A2'}} to="/register">Registrar</Link></li>
+                        <li><Link style={{color: '#A2A2A2'}} to="/register">Equipe</Link></li>
+                        <li><Link style={{color: '#A2A2A2'}} to="/register">FAQ</Link></li>
+                    </ul>
+                </div>
+                <div className="services">
+                    <h2 className='h2Styled'>Produtos</h2>
+                    <ul>
+                        <li><Link style={{color: '#A2A2A2'}} to="#">Camisetas</Link></li>
+                        <li><Link style={{color: '#A2A2A2'}} to="#">Moletons</Link></li>
+                    </ul>
+                </div>
+            </div>
         </div>
-      </div>
-      <hr />
-      <ul className="list-unstyled list-inline text-center py-2">
-        <li className="list-inline-item">
-          <h5 className="mb-1">Conheça o nosso site!</h5>
-        </li>
-        <li className="list-inline-item">
-          <a href="Cadastro" className="btn btn-outline-danger btn-rounded">
-            Registrar-se
-          </a>
-        </li>
-      </ul>
-      <hr />
-      <div className="hover-effect1">
-        <ul style={{ backgroundColor: '#000' }} className="list-unstyled list-inline text-center">
-          <li className="list-inline-item">
-            <a href="https://www.facebook.com/codewithfaraz" title="Facebook">
-              <i className="fa fa-facebook"></i>
-            </a>
-          </li>
-          <li className="list-inline-item">
-            <a href="https://www.twitter.com/codewithfaraz" title="Twitter">
-              <i className="fa fa-twitter"></i>
-            </a>
-          </li>
-          <li className="list-inline-item">
-            <a href="https://www.instagram.com/codewithfaraz" title="Instagram">
-              <i className="fa fa-instagram"></i>
-            </a>
-          </li>
-          <li className="list-inline-item">
-            <a href="https://www.youtube.com/@codewithfaraz" title="Youtube">
-              <i className="fa fa-youtube"></i>
-            </a>
-          </li>
-          <li className="list-inline-item">
-            <a href="#." title="Github">
-              <i className="fa fa-github"></i>
-            </a>
-          </li>
-        </ul>
-      </div>
-      <div style={{ backgroundColor: '#b3b3cc' }} className="footer-copyright text-center py-3">
-        Copyright© 2023: Made by Guilherme Moraes and Gabriel Andreoli
-      </div>
-    </footer>
-  );
-}
+    );
+};
 
 export default Footer;
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -24,26 +24,31 @@ const Header: React.FC = () => {
             <p className='text-underline'>CAGI</p>
           </a>
           <a href="/" className='mx-2'>
-          <p className='text-underline'>HOME</p>
+            <p className='text-underline'>HOME</p>
           </a>
           <a href="/produtos" className='mx-2'>
-          <p className='text-underline'>PRODUTOS</p>
+            <p className='text-underline'>PRODUTOS</p>
           </a>
           <a href="/sobre" className='mx-2'>
-          <p className='text-underline'>SOBRE O CURSO</p>
+            <p className='text-underline'>SOBRE O CURSO</p>
           </a>
         </div>
       </nav>
       <div className="right-side">
-      <a href='Login' className="login-link">
-            <span>LOGAR</span>
-            <LogIn className="login-icon" />
-          </a>
-          <ShoppingCart />
-        </div>
+  <a href='Login' className="login-link">
+    <span className="login-text">LOGAR</span>
+    <LogIn className="login-icon" />
+  </a>
+  <a href='mycart' className="cart-link">
+    <ShoppingCart className="cart-icon" />
+  </a>
+</div>
+
+
+
     </header>
   );
 }
 
 export default Header;
-export {};
+export { };
